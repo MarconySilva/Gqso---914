4d29e89bb9038bfdee522c5c4f98b284fd528cfc
MarconySilva <msf4@aluno.ifal.edu.br>
2021-12-01 12:34:19 +0000

    Criação do teste de potencia

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	// Utilizando o framework fiber.
+	// https://github.com/gofiber/fiber
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Bem-vindo(a) a calculadora API.")
+	})
+
+	registerHandlers(app)
+
+	app.Listen(":8000")
+}
+
+func registerHandlers(app *fiber.App) {
+	app.Get("/potencia/:base/:exp", potenciaHandler)
+}
